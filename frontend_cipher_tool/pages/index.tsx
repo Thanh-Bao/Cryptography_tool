af638b0553d740038c17800ad6066dadd3c5a5c2
Thanh Bao <thanh.bao@outlook.com>
2021-10-24 18:42:51 +0700

    update

--- frontend_cipher_tool/pages/index.tsx
+++ frontend_cipher_tool/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
 import Layout from '@/layout/layout'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
