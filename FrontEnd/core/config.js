c64a9972f3e193764b6a11692591335cc2331f47
Thanh Bao <thanh.bao@outlook.com>
2021-11-23 00:41:51 +0700

    https

--- FrontEnd/core/config.js
+++ FrontEnd/core/config.js
@@ -1,3 +1,3 @@
 // export const SITE_URL = "http://localhost";
-export const SITE_URL = "http://api.baobaocipher.xyz";
+export const SITE_URL = "https://api.baobaocipher.xyz";
 //  export const SITE_URL = "http://localhost:8080";
